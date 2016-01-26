63340fe9e81a444a8f20f3a1ce461ed9c5756586
Weikun <weikun_zhuang@umail.ucsb.edu>
2016-01-25 23:02:12 -0800

    added updated version of SimpleGUI1 to my src directory - done with Step 5

--- src/edu/ucsb/cs56/w16/drawings/wkzhuang/SimpleGui1.java
+++ src/edu/ucsb/cs56/w16/drawings/wkzhuang/SimpleGui1.java
@@ -1,4 +1,4 @@
-package edu.ucsb.cs56.w16.drawings.pconrad;
+package edu.ucsb.cs56.w16.drawings.wkzhuang;
 
 import javax.swing.*;
 
@@ -20,9 +20,9 @@ public class SimpleGui1 {
     public static void main (String[] args) {
 	JFrame frame = new JFrame() ;
 	
-	JButton button = new JButton("Click me") ;
+	JButton button = new JButton("Click me like you mean it") ;
 	
-	java.awt.Color myColor = new java.awt.Color(204,255,000);   // R, G, B values.
+	java.awt.Color myColor = new java.awt.Color(000,153,255);   // R, G, B values.
 	button.setBackground(myColor);
 	button.setOpaque(true);
 	
