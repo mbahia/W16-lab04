196c960069556fdc68991cb452a621e33938491b
Marvin Shu <marvinshu@umail.ucsb.edu>
2016-01-26 11:29:33 -0800

    added snowman's head

--- src/edu/ucsb/cs56/w16/drawings/marvinshu/simple/PictureComponent.java
+++ src/edu/ucsb/cs56/w16/drawings/marvinshu/simple/PictureComponent.java
@@ -13,7 +13,7 @@ import java.awt.geom.Rectangle2D;
    A component that draws a Picture by Phill Conrad
    
    @author Phill Conrad (original drawing)
-   @author @@@ ADD YOUR NAME (fixed the snowmans's head)
+   @author Marvin Shu (fixed the snowmans's head)
    @version for UCSB CS56, W16
 */
 
@@ -89,7 +89,13 @@ public class PictureComponent extends JComponent
         g2.draw(snowManMiddleCircle);
 	
         // @@@ ADD CODE HERE TO DRAW THE TOP CIRCLE
-
+	Circle snowManTopCircle =
+	    new Circle(
+		       snowManCenterBottomX,
+		       snowManCenterBottomY-bottomRadius*2-
+		       middleRadius*2-topRadius,
+		       topRadius);
+	g2.draw(snowManTopCircle);
 
 
         // @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
