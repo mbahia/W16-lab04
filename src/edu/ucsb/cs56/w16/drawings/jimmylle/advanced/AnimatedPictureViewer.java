9711a5af9e4e4194e2f97a86406f1952088dbe65
Jimmy Le <jimmylle@umail.ucsb.edu>
2016-02-03 21:01:26 -0800

    Add animation of 2 swords

--- src/edu/ucsb/cs56/w16/drawings/jimmylle/advanced/AnimatedPictureViewer.java
+++ src/edu/ucsb/cs56/w16/drawings/jimmylle/advanced/AnimatedPictureViewer.java
@@ -5,9 +5,13 @@ import java.awt.*;
 
 public class AnimatedPictureViewer extends JFrame {
     Animation animation;
-    Sword sword;
+    Sword sword1;
+    Sword sword2;
+    DrawPanel panel;   
     int x = 100;
     int y = 250;
+    int dx = 5;
+    int dy = 5;
 
     public static void main (String[] args) {
         AnimatedPictureViewer apv = new AnimatedPictureViewer();
@@ -19,20 +23,21 @@ public class AnimatedPictureViewer extends JFrame {
         this.setSize(640,480);
         this.setVisible(true);
 
-        animation = new Animation();
-        Sword sword = new Sword(x,y,25,100);
+        panel = new DrawPanel();
+        this.getContentPane().add(panel);
 
+        animation = new Animation();
         this.getContentPane().addMouseListener(new MouseAdapter() {
             public void mouseEntered(MouseEvent e) {
                 System.out.println("Mouse entered");
-                Get a new animation thread and start it
+                // Get a new animation thread and start it
                 animation = new Animation();
                 animation.start();
             }
 
             public void mouseExited(MouseEvent e) {
                 System.out.println("Mouse exited");
-                Interrupt the aniamation
+                // Interrupt the aniamation
                 animation.interrupt();
                 while(animation.isAlive()){}
                 animation = null;
@@ -40,9 +45,43 @@ public class AnimatedPictureViewer extends JFrame {
         });
     }
 
+    class DrawPanel extends JPanel {
+      public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.RED);
+
+        sword1 = new Sword(x,y,25,100);
+        sword2 = new Sword(100,y,25,100);
+        g2.draw(sword1);
+        g2.draw(sword2);
+      }
+    }
+
     class Animation extends Thread {
         public void run() {
-          
+          try {
+            while (true) {
+              if (x >= 400) {dx = -5;}
+              if (x <= 50) {dx = 5;}
+              if (y >= 300) {dy = -5;}
+              if (y <= 100) {dy = 5;}
+
+              x += dx;
+              y += dy;
+              panel.repaint();
+              Thread.sleep(10);
+            }
+          }
+          catch (Exception ex) {
+            if (ex instanceof InterruptedException) {
+
+            }
+            else {
+              ex.printStackTrace();
+              System.exit(1);
+            }
+          }
         }
     }
 
