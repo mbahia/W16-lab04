09395184f267c9de6d1ecf285020e53890a73892
Jimmy Le <jimmylle@umail.ucsb.edu>
2016-02-08 00:00:47 -0800

    Shorten method for drawing sword

--- src/edu/ucsb/cs56/w16/drawings/jimmylle/advanced/AnimatedPictureComponent.java
+++ src/edu/ucsb/cs56/w16/drawings/jimmylle/advanced/AnimatedPictureComponent.java
@@ -12,15 +12,14 @@ import edu.ucsb.cs56.w16.drawings.utilities.ShapeTransforms;
 public class AnimatedPictureComponent extends JComponent {
     private Shape leftSword;
     private Shape rightSword;
+    private int count = 0;
+    private double yPos = 250;
     private double width = 25;
     private double height = 125;
     private double leftXPos = 50;
     private double rightXPos = 550;
-    private double yPos = 250;
     private double leftAngle = .025 * Math.PI;
     private double rightAngle = -.025 * Math.PI;
-    private int leftCount = 0;
-    private int rightCount = 0;
     
     
     public AnimatedPictureComponent() {
@@ -31,69 +30,44 @@ public class AnimatedPictureComponent extends JComponent {
     public void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
         g2.setColor(Color.BLACK);
-        drawLeftSword(g2);
-        drawRightSword(g2);
+        drawSwords(g2);
     }
 
-    private void drawLeftSword(Graphics2D g2) {
+    private void drawSwords(Graphics2D g2) {
         g2.draw(leftSword);
+        g2.draw(rightSword);
         if (leftXPos != 215) {
             leftXPos += 5;
             leftSword = new Sword(leftXPos, yPos, width, height);
-        }
-        else {
-            if (leftCount <= 11) {
-                leftAngle += .1;
-            }
-            else if (leftCount <= 24) {
-                leftAngle -= .1;
-            }
-            else if (leftCount <= 35) {
-                leftAngle += .1;
-            }
-            leftCount++;
-            leftSword = ShapeTransforms.rotatedCopyOf(new Sword(leftXPos, yPos, width, height), leftAngle);
-            if (leftCount > 35) {
-                leftXPos = 50;
-                leftCount = 0;
-                leftAngle = .025 * Math.PI;
-            }
-        }
-    }
-
-
-    private void drawRightSword(Graphics2D g2) {
-        g2.draw(rightSword);
-        if (rightXPos != 385) {
             rightXPos += -5;
             rightSword = new Sword(rightXPos, yPos, width, height);
         }
         else {
-            if (rightCount <= 11) {
+            if (count <= 11) {
+                leftAngle += .1;
                 rightAngle -= .1;
             }
-            else if (rightCount <= 24) {
+            else if (count <= 24) {
+                leftAngle -= .1;
                 rightAngle += .1;
             }
-            else if (rightCount <= 35) {
+            else if (count <= 35) {
+                leftAngle += .1;
                 rightAngle -= .1;
             }
-            rightCount++;
-            rightSword = ShapeTransforms.rotatedCopyOf(new Sword(rightXPos, yPos, width, height), rightAngle);
-            if (rightCount > 35) {
+            count++;
+            leftSword = ShapeTransforms.rotatedCopyOf(
+                        new Sword(leftXPos, yPos, width, height), leftAngle);
+            rightSword = ShapeTransforms.rotatedCopyOf(
+                        new Sword(rightXPos, yPos, width, height), rightAngle);
+            if (count > 35) {
+                leftXPos = 50;
+                count = 0;
+                leftAngle = .025 * Math.PI;
                 rightXPos = 550;
-                rightCount = 0;
                 rightAngle = -.025 * Math.PI;
             }
         }
     }
-
-
-
-
-
-
-
-
 
 }
