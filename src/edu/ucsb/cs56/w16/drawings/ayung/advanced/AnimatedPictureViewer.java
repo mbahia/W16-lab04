e27268dd326d92926f1d719fe74aac1951bce114
Angela Yung <ayung@umail.ucsb.edu>
2016-02-12 02:49:53 -0800

    add pin animation that turns voodoo doll red if pin point contained in doll

--- src/edu/ucsb/cs56/w16/drawings/ayung/advanced/AnimatedPictureViewer.java
+++ src/edu/ucsb/cs56/w16/drawings/ayung/advanced/AnimatedPictureViewer.java
@@ -16,7 +16,7 @@ import edu.ucsb.cs56.w16.drawings.utilities.GeneralPathWrapper;
 
 
 public class AnimatedPictureViewer {
-
+    
     private DrawPanel panel = new DrawPanel();
     Thread anim;   
     
@@ -24,83 +24,107 @@ public class AnimatedPictureViewer {
     private int y = 100; //y coor
     private int h = 100; //height
 
+    
     Shape test = new VoodooDoll(x,y,h);  //instantiate test doll
+
+
+    private int px = -1;
+    private int py = -1;
+    private int ph = 5;
+    Shape pin;
     
     private int dx = 5;
     private double swing_angle = .015;
-
+    
     public static void main (String[] args) {
-      new AnimatedPictureViewer().go();
+	new AnimatedPictureViewer().go();
     }
-
+    
     public void go() {
-      JFrame frame = new JFrame();
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	JFrame frame = new JFrame();
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	frame.getContentPane().add(panel);
+	frame.setSize(640,480);
+	frame.setVisible(true);
+	
+	frame.getContentPane().addMouseListener(new MouseAdapter() {
+		public void mouseEntered(MouseEvent e){
+		    System.out.println("mouse entered");
+		    anim = new Animation();
+		    anim.start();
+		}
+		
+		public void mouseExited(MouseEvent e){        
+		    System.out.println("Mouse exited");
+		    // Kill the animation thread
+		    anim.interrupt();
+		    while (anim.isAlive()){}
+		    anim = null;         
+		    panel.repaint();
+		}
 
-      frame.getContentPane().add(panel);
-      frame.setSize(640,480);
-      frame.setVisible(true);
-      
-      frame.getContentPane().addMouseListener(new MouseAdapter() {
-        public void mouseEntered(MouseEvent e){
-        System.out.println("mouse entered");
-          anim = new Animation();
-          anim.start();
-        }
-
-        public void mouseExited(MouseEvent e){        
-          System.out.println("Mouse exited");
-          // Kill the animation thread
-          anim.interrupt();
-          while (anim.isAlive()){}
-          anim = null;         
-          panel.repaint();        
-        }
-      });
-      
-    } // go()
-
+		public void mouseClicked(MouseEvent e) {
+		    px = e.getX();
+		    py = e.getY();
+		    pin = new Pin(px,py,ph);
+		}
+	    });
+	
+    }
+    
     class DrawPanel extends JPanel {
-       public void paintComponent(Graphics g) {
+	public void paintComponent(Graphics g) {
+	    
+	    Graphics2D g2 = (Graphics2D) g;
+	    
+	    // Clear the panel first
+	    g2.setColor(Color.white);
+	    g2.fillRect(0,0,this.getWidth(), this.getHeight());
+	    
+	    // Draw the Ipod
+	    g2.setColor(Color.BLACK);
 
-        Graphics2D g2 = (Graphics2D) g;
+	    if(px >= 0) {
+		g2.draw(pin);
+		if(test.contains(px,py)) {
+		    g2.setColor(Color.RED);
+		}
+	    }
 
-         // Clear the panel first
-          g2.setColor(Color.white);
-          g2.fillRect(0,0,this.getWidth(), this.getHeight());
-
-          // Draw the Ipod
-          g2.setColor(Color.BLACK);
-          g2.draw(test);
-       }
+	    g2.draw(test);
+	}
     }
     
     class Animation extends Thread {
-      public void run() {
-        try {
-          while (true) {
-            // Bounce off the walls
-	      /*
-            if (x >= 400) { dx = -5; }
-            if (x <= 50) { dx = 5; }
-            
-            x += dx;
-	      */
-	      //test = new VoodooDoll(x,y,h);
-
-	      test = ShapeTransforms.rotatedCopyOf(test,swing_angle);
-            panel.repaint();
-            Thread.sleep(50);
-          }
-        } catch(Exception ex) {
-          if (ex instanceof InterruptedException) {
-            // Do nothing - expected on mouseExited
-          } else {
-            ex.printStackTrace();
-            System.exit(1);
-          }
-        }
-      }
+	public void run() {
+	    try {
+		while (true) {
+		    // Bounce off the walls
+		    /*
+		      if (x >= 400) { dx = -5; }
+		      if (x <= 50) { dx = 5; }
+		      
+		      x += dx;
+		    */
+		    //test = new VoodooDoll(x,y,h);
+		    
+		    test = ShapeTransforms.rotatedCopyOf(test,swing_angle);
+		    if(px >= 0) {
+			pin = ShapeTransforms.rotatedCopyOf(pin, swing_angle);
+		    }
+		    panel.repaint();
+		    Thread.sleep(20);
+		}
+	    } catch(Exception ex) {
+		if (ex instanceof InterruptedException) {
+		    // Do nothing - expected on mouseExited
+		} else {
+		    ex.printStackTrace();
+		    System.exit(1);
+		}
+	    }
+	}
     }
     
 }
